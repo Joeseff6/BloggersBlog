b53595008bfa42f99571deee289f4452c4d92d8b
Joseph Soria <josephsoria6@gmail.com>
2021-03-15 00:29:16 -0500

    commented lines to ensure server.js worked

--- server.js
+++ server.js
@@ -1,24 +1,27 @@
 const path = require('path');
 const express = require('express');
-const exphbs = require('express-handlebars');
-const routes = require();
+// const exphbs = require('express-handlebars');
+// const routes = require();
 // const helpers = require();
 const sequelize = require('./config/connection');
+const { Comments, Post, User} = require(`./models`);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create();
+// const hbs = exphbs.create();
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+// app.engine('handlebars', hbs.engine);
+// app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(routes);
+// app.use(routes);
 
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+sequelize.sync().then(() => {
+  app.listen(PORT, () => console.log('Now listening on port 3001'));
 });
+
+
