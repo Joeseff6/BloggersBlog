503f14443ce93c078dfe5adf82c39dd1de6781e5
Joseph Soria <josephsoria6@gmail.com>
2021-03-14 22:59:26 -0500

    Completed Comments model

---
+++ models/Comments.js
@@ -0,0 +1,43 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comments extends Model {};
+
+Comments.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        date: {
+            type: DataTypes.DATEONLY,
+            allowNull: false,
+        },
+        text: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            references: {
+                model: `post`,
+                key: `id`,
+            },
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: `user`,
+                key: `id`,
+            },
+        },
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'comments',
+    }
+);
+
+module.exports = Comments;
