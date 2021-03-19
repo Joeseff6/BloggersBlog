6b910ea67c984566f875284f4987b66978e755f7
Joseph Soria <josephsoria6@gmail.com>
2021-03-18 19:26:23 -0500

    Finished writing front end logic for creating new user

---
+++ public/js/newUser.js
@@ -0,0 +1,28 @@
+const newUserHandler = async (event) => {
+    event.preventDefault();
+    
+    const username = document.getElementById(`name`).value.trim();
+    const email = document.getElementById(`email`).value.trim();
+    const password = document.getElementById(`password`).value.trim();
+
+
+    if (username && email && password) {
+        const response = await fetch('/api/users', {
+            method: 'POST',
+            body: JSON.stringify({ username, email, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+        
+        if (response.ok) {
+            document.location.replace('/posts');
+        } else {
+            document.getElementById(`tryAgain`).innerHTML = `Something went wrong. 
+            Please check your information.`
+        }
+    };
+};
+
+
+document
+.getElementById(`newUserForm`)
+.addEventListener(`submit`, newUserHandler)
