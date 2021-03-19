4be1f528ec231fef4a16019c1a807b4a117cc28d
Joseph Soria <josephsoria6@gmail.com>
2021-03-18 19:59:49 -0500

    Finished front end logic for logging in

--- public/js/login.js
+++ public/js/login.js
@@ -1,14 +1,24 @@
 const loginHandler = async (event) => {
     event.preventDefault();
-    // TODO: Fix ids in login view to match below variables 
     const email = document.getElementById(`email`).value.trim();
     const password = document.getElementById(`password`).value.trim();
 
-    if (username && email && password) {
-        const response = await fetch('/api/users', {
+    if (email && password) {
+        const response = await fetch('/api/users/login', {
             method: 'POST',
-            body: JSON.stringify({ username, email, password }),
+            body: JSON.stringify({ email, password }),
             headers: { 'Content-Type': 'application/json' },
         });
+
+        if (response.ok) {
+            document.location.replace('/posts');
+        } else {
+            document.getElementById(`wrongUser`).innerHTML = `Couldn't find that user, 
+            please try again.`
+        }
     };
 };
+
+document
+.getElementById(`loginForm`)
+.addEventListener(`submit`, loginHandler)
