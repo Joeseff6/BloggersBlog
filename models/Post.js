5fb5110835ffe79985e49438830762e158fd1a4f
Joseph Soria <josephsoria6@gmail.com>
2021-03-21 11:39:15 -0500

    Added text key and update posted_date datatype

--- models/Post.js
+++ models/Post.js
@@ -6,21 +6,26 @@ class Post extends Model {};
 Post.init(
     {
         id: {
-                type: DataTypes.INTEGER,
-                allowNull: false,
-                primaryKey: true,
-                autoIncrement: true,
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
         },
         title: {
             type: DataTypes.STRING,
             allowNull: false,
         },
+        text: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
         posted_date: {
-            type: DataTypes.DATEONLY,
+            type: DataTypes.STRING,
             allowNull: false,
         },
         user_id: {
             type: DataTypes.INTEGER,
+            allowNull: false,
             references: {
                 model: `user`,
                 key: `id`,
