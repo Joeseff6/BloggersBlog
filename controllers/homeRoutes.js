1d3b9262ef1dbeebc01037e3f6dc2097c2af66c6
Joseph Soria <josephsoria6@gmail.com>
2021-03-21 11:37:56 -0500

    Wrote get request to retrieve posts

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,15 +1,51 @@
 const router = require('express').Router();
+const { User, Post } = require(`../models`)
 
 router.get(`/`, async (req,res) => {
-    res.render(`homepage`)
+    console.log(req.session)
+    if (req.session.loggedIn) {
+        res.render(`posts`)
+    } else {
+        res.render(`homepage`, {
+            loggedIn: req.session.loggedIn
+        })
+    }
 })
 
 router.get(`/login`, async (req,res) => {
-    res.render(`login`)
+        res.render(`login`)
 })
 
 router.get(`/newUser`, async (req,res) => {
     res.render(`newUser`)
+})
+
+router.get(`/posts`, async (req,res) => {
+    try {
+        const postData = await Post.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: [`username`],
+                },
+            ],
+        })
+        const posts = postData.map(post => {
+            return post.get({plain: true})
+        })
+        console.log(posts)
+        res.render(`posts`, {
+            posts,
+            loggedIn: req.session.loggedIn,
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+})
+
+router.get(`/newPost`, async (req,res) => {
+    res.render(`newPost`)
 })
 
 
