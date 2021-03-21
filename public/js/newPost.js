45373e4dcfbc2147e4ce6dfc307fd2e6d7fc2f82
Joseph Soria <josephsoria6@gmail.com>
2021-03-21 11:41:12 -0500

    Wrote logic for when user submits post request when submitting form

---
+++ public/js/newPost.js
@@ -0,0 +1,20 @@
+
+const createPost = async (event) => {
+    event.preventDefault();
+    const title = document.getElementById(`title`).value.trim()
+    const text = document.getElementById(`postText`).value.trim()
+    const response = await fetch(`/api/users/newPost`, {
+        method: `POST`,
+        body: JSON.stringify({ title, text }),
+        headers: { 'Content-Type': 'application/json' },
+    })
+
+    if (response.ok) {
+        document.location.replace('/posts');
+    }
+
+}
+
+document
+.getElementById(`postForm`)
+.addEventListener(`submit`, createPost)
