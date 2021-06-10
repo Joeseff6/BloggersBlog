4f31898ed3d6f74abb29b022afd3d14acd6820b8
Joseph Soria <josephsoria6@gmail.com>
2021-06-09 22:53:35 -0500

    reformatter using Prettier

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,88 +1,85 @@
-const router = require('express').Router();
-const { User, Post, Comments } = require(`../models`)
+const router = require("express").Router();
+const { User, Post, Comments } = require(`../models`);
 
-router.get(`/`, async (req,res) => {
-
-    res.render(`homepage`, {
-        loggedIn: req.session.loggedIn
-    });
+router.get(`/`, async (req, res) => {
+  res.render(`homepage`, {
+    loggedIn: req.session.loggedIn,
+  });
 });
 
-router.get(`/login`, async (req,res) => {
-        res.render(`login`);
+router.get(`/login`, async (req, res) => {
+  res.render(`login`);
 });
 
-router.get(`/newUser`, async (req,res) => {
-    res.render(`newUser`);
+router.get(`/newUser`, async (req, res) => {
+  res.render(`newUser`);
 });
 
-router.get(`/posts`, async (req,res) => {
-    try {
-        if (!req.session.loggedIn) {
-            res.render(`notLogged`);
-        } else {
-            const postData = await Post.findAll({
-                include: 
-                    {
-                        model: User,
-                        attributes: [`username`],
-                    },
-            });
-            const posts = postData.map(post => {
-                return post.get({plain: true});
-            });
-            res.render(`posts`, {
-                posts,
-                loggedIn: req.session.loggedIn,
-            });
-        };
-    } catch (err) {
-        console.log(err);
-        res.status(500).json(err);
+router.get(`/posts`, async (req, res) => {
+  try {
+    if (!req.session.loggedIn) {
+      res.render(`notLogged`);
+    } else {
+      const postData = await Post.findAll({
+        include: {
+          model: User,
+          attributes: [`username`],
+        },
+      });
+      const posts = postData.map((post) => {
+        return post.get({ plain: true });
+      });
+      res.render(`posts`, {
+        posts,
+        loggedIn: req.session.loggedIn,
+      });
     }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
-router.get(`/posts/:id`, async (req,res) => {
-    try {
-        req.session.postId = req.params.id;
-        if (!req.session.loggedIn) {
-            res.render(`notLogged`);
-        } else {
-            const postData = await Post.findByPk(req.params.id, {
-                include: [
-                    {
-                        model: User,
-                        attributes: [`username`],
-                    },
-                    {
-                        model: Comments,
-                        include: [
-                                {
-                                model: User,
-                                attributes: [`username`],
-                            },
-                        ]
-                    },
-                ]
-            });
-            const posts = postData.get({plain: true});
-            console.log(posts);
-            res.render(`view`, { posts, loggedIn: req.session.loggedIn });
-        };
-    } catch (err) {
-        console.log(err);
-        res.status(500).json(err);
+router.get(`/posts/:id`, async (req, res) => {
+  try {
+    req.session.postId = req.params.id;
+    if (!req.session.loggedIn) {
+      res.render(`notLogged`);
+    } else {
+      const postData = await Post.findByPk(req.params.id, {
+        include: [
+          {
+            model: User,
+            attributes: [`username`],
+          },
+          {
+            model: Comments,
+            include: [
+              {
+                model: User,
+                attributes: [`username`],
+              },
+            ],
+          },
+        ],
+      });
+      const posts = postData.get({ plain: true });
+      console.log(posts);
+      res.render(`view`, { posts, loggedIn: req.session.loggedIn });
     }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
-router.get(`/newPost`, async (req,res) => {
-    try {
-        res.render(`newPost`, {loggedIn: req.session.loggedIn});
-    } catch (err) {
-        console.log(err);
-        res.status(500).json(err);
-    }
+router.get(`/newPost`, async (req, res) => {
+  try {
+    res.render(`newPost`, { loggedIn: req.session.loggedIn });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
-
 
 module.exports = router;
--- models/Comments.js
+++ models/Comments.js
@@ -1,47 +1,47 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
 
-class Comments extends Model {};
+class Comments extends Model {}
 
 Comments.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        date: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        comment: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        post_id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            references: {
-                model: `post`,
-                key: `id`,
-            },
-        },
-        user_id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            references: {
-                model: `user`,
-                key: `id`,
-            },
-        },
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-        sequelize,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'comments',
-    }
+    date: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    comment: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: `post`,
+        key: `id`,
+      },
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: `user`,
+        key: `id`,
+      },
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "comments",
+  }
 );
 
 module.exports = Comments;
