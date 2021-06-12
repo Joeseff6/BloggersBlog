d77f9f01459ae146a94b501daeaeb760df4967e7
Joseph Soria <josephsoria6@gmail.com>
2021-06-12 12:41:33 -0500

    separated post routes from user

---
+++ controllers/api/postRoutes.js
@@ -0,0 +1,26 @@
+const router = require(`express`).Router();
+const date = new Date();
+const today =
+`${date.getMonth() + 1}` +
+`/` +
+`${date.getDate()}` +
+`/` +
+`${date.getFullYear()}`;
+
+// Create new post
+router.post(`/`, (req, res) => {
+  try {
+    const postData = Post.create({
+      title: req.body.title,
+      text: req.body.text,
+      posted_date: today,
+      user_id: req.session.userId,
+    });
+    res.status(200).json(postData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
