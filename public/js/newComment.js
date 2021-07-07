f51adc3f7269bb6db8563b9890ed5600d7be6c32
Joseph Soria <josephsoria6@gmail.com>
2021-07-06 23:17:31 -0500

    added postId to body, changed page to reload

--- public/js/newComment.js
+++ public/js/newComment.js
@@ -2,22 +2,22 @@ const commentBtn = document.getElementById(`commentBtn`);
 const commentForm = document.getElementById(`commentForm`);
 
 commentBtn.addEventListener(`click`, () => {
-    commentForm.style.display = "block"
-})
+  commentForm.style.display = "block";
+});
 
+const postComment = async (event) => {
+  event.preventDefault();
+  const comment = document.getElementById(`commentText`).value.trim();
+  const urlArr = window.location.pathname.split("/");
+  const postId = urlArr[urlArr.length - 1];
+  const response = await fetch(`/api/comments/`, {
+    method: `POST`,
+    body: JSON.stringify({ comment, postId }),
+    headers: { "Content-Type": "application/json" },
+  });
+  if (response.ok) {
+    document.location.reload();
+  }
+};
 
-const postComment = (event) => {
-    event.preventDefault();
-    const comment = document.getElementById(`commentText`).value.trim();
-    const response = fetch(`/api/comments/`, {
-        method: `POST`,
-        body: JSON.stringify({ comment }),
-        headers: { 'Content-Type': 'application/json' },
-    })
-
-    if (response.ok) {
-        document.location.reload();
-    }
-}
-
-commentForm.addEventListener(`submit`, postComment)
+commentForm.addEventListener(`submit`, postComment);
